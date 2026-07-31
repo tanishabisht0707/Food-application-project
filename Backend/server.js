ce7f5cff86205d3265e6b3d91d96873567b37166
Tanisha Bisht <your-github-email@example.com>
2026-07-31 17:32:46 +0530

    backend updated code

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import axios from 'axios';
 import cors from 'cors';
-import "dotenv/config";  
+// env 
+import "dotenv/config";
+  
 const app = express();
 
 app.use(cors());
